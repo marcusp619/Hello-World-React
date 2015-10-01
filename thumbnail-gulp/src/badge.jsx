2a0cb9ae52df095eba4d9ddc2f08511f60359222
MARK PEARYER <markpearyer@MARKs-Air.attlocal.net>
2015-10-01 09:13:23 -0700

    Required gulp packages

---
+++ thumbnail-gulp/src/badge.jsx
@@ -0,0 +1,11 @@
+// Badge Class
+var Badge = React.createClass({
+  //Must have render
+  render: function() {
+    return (
+      <button className="btn btn-primary" type="button">
+        {this.props.title} <span className="badge">{this.props.number}</span>
+      </button>
+    );
+  }
+});
---
+++ thumbnail-gulp/src/thumbnailList.jsx
@@ -0,0 +1,14 @@
+var ThumbnailList = React.createClass({
+  render: function() {
+    //points at the ThumbnailData array and maps through it
+    var list = this.props.ThumbnailData.map(function(thumbnailProps) {
+      // making multiple thumbnails
+      //... passes in all the objects properties
+      return <Thumbnail {...thumbnailProps} />
+    });
+    // rendering to page..
+    return <div>
+      {list}
+    </div>
+  }
+});
